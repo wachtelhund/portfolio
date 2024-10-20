11841a2bd4dadd3ed99734d354b0cfd12a72072b
wachtelhund <hampus.nilsson222@hotmail.se>
2024-10-20 18:31:16 +0200

    Add owl carousel

--- src/app/components/project-card-list/project-card-list.component.ts
+++ src/app/components/project-card-list/project-card-list.component.ts
@@ -1,35 +1,48 @@
 import { Component, ElementRef, input, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o'
+import { Repo } from '../../services/repo.model';
+import { ProjectCardComponent } from '../project-card/project-card.component';
 
 @Component({
   selector: 'app-project-card-list',
   standalone: true,
   imports: [
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    CarouselModule,
+    ProjectCardComponent
   ],
   templateUrl: './project-card-list.component.html',
   styleUrl: './project-card-list.component.scss'
 })
 export class ProjectCardListComponent {
   title = input<string>();
+  repos = input<Repo[]>();
 
-  @ViewChild('carousel') carousel!: ElementRef;
-
-  @ViewChild('card') card!: ElementRef;
-
-  cardWidth: number = 0;
-
-  ngAfterViewInit(): void {
-    this.cardWidth = this.card.nativeElement.offsetWidth;
-  }
-
-  scrollLeft(): void {
-    this.carousel.nativeElement.scrollBy({ left: -this.cardWidth, behavior: 'smooth' });
-  }
-
-  scrollRight(): void {
-    this.carousel.nativeElement.scrollBy({ left: this.cardWidth, behavior: 'smooth' });
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: false,
+    dots: true,
+    navSpeed: 700,
+    navText: [],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 1
+      },
+      740: {
+        items: 1
+      },
+      940: {
+        items: 2
+      }
+    },
+    // nav: true
   }
 }
--- src/app/components/project-card/project-card.component.ts
+++ src/app/components/project-card/project-card.component.ts
@@ -2,14 +2,17 @@ import { Component, input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { Repo } from '../../services/repo.model';
+import { CommonModule, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-project-card',
   standalone: true,
   imports: [
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    CommonModule
   ],
+  providers: [],
   templateUrl: './project-card.component.html',
   styleUrl: './project-card.component.scss'
 })
